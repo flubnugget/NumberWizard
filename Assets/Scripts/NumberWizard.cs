3cf051156b2d0ec67045921386e3eb49fb1b0b1f
flubnugget <32379773+flubnugget@users.noreply.github.com>
2018-01-30 17:01:24 -1000

    Finished Number Wizard

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,37 +4,59 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+
+    void Start () {
+        StartGame () ;
+	}
+
+    void StartGame ()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+       
+        print("_________________________");
         print("Welcome to Number Wizard");
         print("QUICK! PICK A NUMBER IN YOUR SKULL!");
 
-        int max = 1000;
-        int min = 1;
-
-        print("The highest number you can pick is" + max);
+        print("The highest number you can pick is " + max);
         print("The lowest number you can pick is " + min);
 
-        print("Is the number higher or lower than 500?");
+        print("Is the number higher or lower than " + guess + "?");
         print("Up = Higher, Down = Lower, Return = Equal");
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            print("Up arrow is pressed");
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            print("Down arrow is pressed");
-        }
+        max = max + 1;
+    }
 
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
         {
             print("I won! Mwahahaha");
+            StartGame();
         }
-
     }
+
+        void NextGuess () {
+            guess = (max + min) / 2;
+            print("Higher or lower than " + guess + "?");
+            print("Up = Higher, Down = Lower, Return = Equal");
+    }
+   
 }
